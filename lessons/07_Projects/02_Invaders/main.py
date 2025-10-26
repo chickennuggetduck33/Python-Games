7d0d18ce872b34d53d5b84e79ceb0ac825c71f7c
chickennuggetduck33 <107875096+chickennuggetduck33@users.noreply.github.com>
2025-10-26 19:55:01 +0000

    jdbidbrnbijdfnbijdrth

--- lessons/07_Projects/02_Invaders/main.py
+++ lessons/07_Projects/02_Invaders/main.py
@@ -1,16 +1,18 @@
 import pygame
 import math
+import random
 from pathlib import Path
 pygame.init()
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 200, 200
+SCREEN_WIDTH, SCREEN_HEIGHT = 600, 600
 FPS = 60
 assets = Path(__file__).parent / "images"
 images_dir = Path(__file__).parent / "images" if (Path(__file__).parent / "images").exists() else Path(__file__).parent / "assets"
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-WIDTH, HEIGHT = 600, 300
-PLAYER_SIZE = 25
-player_speed = 5
+PLAYER_SIZE = 75
+ENEMY_SPEED = 1
+HEIGHT = 75
+player_speed = 1
 BLUE = (0, 0, 255)
 
 class Player(pygame.sprite.Sprite):
@@ -18,11 +20,11 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load(assets/'spaceship.png')
         print(self.image.get_rect())
-        self.image = pygame.transform.scale(self.image,(25, 25))
+        self.image = pygame.transform.scale(self.image,(75, 75))
         #self.image.fill(settings.BLUE)
         self.rect = self.image.get_rect()
-        self.rect.x = 50
-        self.rect.y = HEIGHT - PLAYER_SIZE - 10
+        self.rect.x = 262.5
+        self.rect.y = HEIGHT - PLAYER_SIZE + 400
         self.speed = player_speed
 player = Player()
 group = pygame.sprite.Group()
@@ -31,7 +33,21 @@ group.add(player)
     # Initial position of the square
     #square_x = SCREEN_WIDTH // 2 - SQUARE_SIZE // 2
     #square_y = SCREEN_HEIGHT // 2 - SQUARE_SIZE // 2
-    
+EX = random.randint(0, SCREEN_WIDTH - PLAYER_SIZE)
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(assets/'alien.png')
+        print(self.image.get_rect())
+        self.image = pygame.transform.scale(self.image,(75, 75))
+        #self.image.fill(settings.BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = EX
+        self.rect.y = 200
+        self.speed = ENEMY_SPEED
+enemy = Enemy()
+Egroup = pygame.sprite.Group()
+Egroup.add(enemy)
 running = True
     
 while running:
@@ -44,7 +60,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     group.draw(screen)
-    screen.fill((255, 0, 0))
+    screen.fill((0, 0, 155))
+    group.draw(screen)
+    Egroup.draw(screen)
+    enemy.rect.y += enemy.speed
+    player.rect.x = max(0, min(SCREEN_WIDTH - PLAYER_SIZE, player.rect.x))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.rect.x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player.rect.x += player_speed
+    pygame.display.flip()
+    
+    
         
         # Get the keys pressed. Gtes an array of all the keys
         # with a boolean value of whether they are pressed or not
